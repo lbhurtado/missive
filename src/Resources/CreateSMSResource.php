f7efe8d71086c9fdd095e45957586aeba5f8e55d
Lester Hurtado <lester@hurtado.ph>
2019-05-14 13:36:17 +0800

    add sms id in CreateSMSResource, get from Missive::getSMS()

--- src/Resources/CreateSMSResource.php
+++ src/Resources/CreateSMSResource.php
@@ -2,6 +2,7 @@
 
 namespace LBHurtado\Missive\Resources;
 
+use LBHurtado\Missive\Missive;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CreateSMSResource extends JsonResource
@@ -9,6 +10,7 @@ class CreateSMSResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => Missive::getSMS()->id,
             'from' => $this->from,
             'to' => $this->to,
             'message' => $this->message,
