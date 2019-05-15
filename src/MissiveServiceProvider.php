dec4409d9bc50d538abe76791ed14a5697c1c07f
Lester Hurtado <lester@hurtado.ph>
2019-05-15 18:14:40 +0800

    refactor in MissiveServiceProvider

--- src/MissiveServiceProvider.php
+++ src/MissiveServiceProvider.php
@@ -20,29 +20,8 @@ class MissiveServiceProvider extends ServiceProvider
         app('missive.sms')::observe(SMSObserver::class);
 
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('missive.php'),
-            ], 'config');
-
-            // Publishing the migrations.
-            if (! class_exists(CreateSMSsTable::class)) {
-                $this->publishes([
-                    __DIR__.'/../database/migrations/create_s_m_s_s_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_s_m_s_s_table.php'),
-                ], 'migrations');                
-            }
-            if (! class_exists(CreateContactsTable::class)) {
-                $this->publishes([
-                    __DIR__.'/../database/migrations/create_contacts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_contacts_table.php'),
-                ], 'migrations');                
-            }
-            if (! class_exists(CreateRelaysTable::class)) {
-                $this->publishes([
-                    __DIR__.'/../database/migrations/create_relays_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_relays_table.php'),
-                ], 'migrations');                
-            }
-        }
+        $this->registerConfigs();
+        $this->publishMigrations();
     }
 
     /**
@@ -55,6 +34,36 @@ class MissiveServiceProvider extends ServiceProvider
         $this->registerModels();
         $this->registerFacades();
         $this->registerClasses();
+    }
+
+    protected function publishConfigs()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('missive.php'),
+            ], 'config');
+        }
+    }
+
+    protected function publishMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            if (! class_exists(CreateSMSsTable::class)) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_s_m_s_s_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_s_m_s_s_table.php'),
+                ], 'migrations');
+            }
+            if (! class_exists(CreateContactsTable::class)) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_contacts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_contacts_table.php'),
+                ], 'migrations');
+            }
+            if (! class_exists(CreateRelaysTable::class)) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_relays_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_relays_table.php'),
+                ], 'migrations');
+            }
+        }
     }
 
     protected function registerConfigs()
