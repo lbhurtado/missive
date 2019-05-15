929118db291940e09ec359464aa32cd83c2199f9
Lester Hurtado <lester@hurtado.ph>
2019-05-15 12:53:56 +0800

    rollback the CreateSMSCommand for clarity

--- src/Commands/CreateSMSCommand.php
+++ src/Commands/CreateSMSCommand.php
@@ -7,39 +7,39 @@ use LBHurtado\Tactician\Contracts\CommandInterface;
 
 class CreateSMSCommand implements CommandInterface
 {
-//    public $from;
-//
-//    public $to;
-//
-//    public $message;
-//
-//    public function __construct($from, $to, $message)
-//    {
-//        $this->from = $from;
-//        $this->to = $to;
-//        $this->message = $message;
-//    }
+    public $from;
 
-    protected $request;
+    public $to;
 
-    public function __construct(Request $request)
+    public $message;
+
+    public function __construct($from, $to, $message)
     {
-        $this->request = $request;
-        foreach ($request as $property => $value) {
-            $this->{$property} = $value;
-        }
+        $this->from = $from;
+        $this->to = $to;
+        $this->message = $message;
     }
+
+//    protected $request;
+//
+//    public function __construct(Request $request)
+//    {
+//        $this->request = $request;
+//        foreach ($request as $property => $value) {
+//            $this->{$property} = $value;
+//        }
+//    }
 
     public function getProperties():array
     {
-//        return [
-//            'from' => $this->from,
-//            'to' => $this->to,
-//            'message' => $this->message,
-//        ];
-        $attributes = $this->request->only(config('tactician.fields'));
-
-        \Log::info($attributes);
-        return $attributes;
+        return [
+            'from' => $this->from,
+            'to' => $this->to,
+            'message' => $this->message,
+        ];
+//        $attributes = $this->request->only(config('tactician.fields'));
+//
+//        \Log::info($attributes);
+//        return $attributes;
     }
 }
