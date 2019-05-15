6477dfcfe44cf336c980958d93261e5ca135f1f5
Lester Hurtado <lester@hurtado.ph>
2019-05-16 00:14:16 +0800

    add SMSFactory 2

--- src/MissiveServiceProvider.php
+++ src/MissiveServiceProvider.php
@@ -4,6 +4,7 @@ namespace LBHurtado\Missive;
 
 use Opis\Events\EventDispatcher;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
 use LBHurtado\Missive\Models\{SMS, Contact, Relay};
 use LBHurtado\Missive\Repositories\{SMSRepository, SMSRepositoryEloquent};
 use LBHurtado\Missive\Observers\{SMSObserver, ContactObserver, RelayObserver};
@@ -18,6 +19,7 @@ class MissiveServiceProvider extends ServiceProvider
         $this->registerConfigs();
         $this->publishMigrations();
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+        $this->app->make(Factory::class)->load(__DIR__ . '/../database/factories');
     }
 
     public function register()
