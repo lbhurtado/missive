44447ef72f95c9492beed7242b791142a49f39cc
Lester Hurtado <lester@hurtado.ph>
2019-05-16 23:43:19 +0800

    add sms repository test

--- tests/SMSTest.php
+++ tests/SMSTest.php
@@ -3,19 +3,27 @@
 namespace LBHurtado\Missive\Tests;
 
 use LBHurtado\Missive\Models\SMS;
+use LBHurtado\Missive\Repositories\SMSRepository;
 
 class SMSTest extends TestCase
 {
     /** @test */
     public function sms_model_has_from_to_messages_fields()
     {
-        $from = '+639171234567';
-        $to = '+639187654321';
-        $message = 'Test Messages';
-
+        $from = '+639171234567'; $to = '+639187654321'; $message = 'Test Messages';
         $sms = SMS::create(compact('from', 'to', 'message'));
+
         $this->assertEquals($from, $sms->from);
         $this->assertEquals($to, $sms->to);
         $this->assertEquals($message, $sms->message);
+    }
+
+    /** @test */
+    public function sms_repository_can_create_sms()
+    {
+        $from = '+639171234567'; $to = '+639187654321'; $message = 'Test Messages';
+        app(SMSRepository::class)->create(compact('from', 'to', 'message'));
+
+        $this->assertDatabaseHas(config('missive.table_names.smss'), compact('from', 'to', 'message'));
     }
 }
