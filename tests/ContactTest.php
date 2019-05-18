bbcc16ea6ec3fd2823ddcd2a39beae5f9c9a8c36
Lester Hurtado <lester@hurtado.ph>
2019-05-18 19:48:46 +0800

    add mobilehandle and event tests in ContactTest

--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -3,6 +3,10 @@
 namespace LBHurtado\Missive\Tests;
 
 use Illuminate\Support\Arr;
+use LBHurtado\Missive\Events\ContactEvent;
+use LBHurtado\Missive\Events\ContactEvents;
+use Opis\Events\EventDispatcher;
+use LBHurtado\Missive\Classes\MobileHandle;
 use LBHurtado\Missive\Models\Contact;
 use Illuminate\Database\QueryException;
 
@@ -45,5 +49,42 @@ class ContactTest extends TestCase
 
         /*** act ***/
         Contact::create($attributes = compact('mobile'));
+    }
+
+    /** @test */
+    public function contact_has_model_factory()
+    {
+        /*** arrange ***/
+        $mobile = '+639171234567';
+
+        /*** act ***/
+        $contact = factory(Contact::class)->create($attributes = compact('mobile'));
+
+        /*** assert ***/
+        $this->assertEquals($contact->id, Contact::where($attributes)->first()->id);
+    }
+
+    /** @test */
+    public function contact_is_mobile_handle()
+    {
+        /*** act ***/
+        $contact = factory(Contact::class)->create();
+
+        /*** assert ***/
+        $this->assertInstanceOf(MobileHandle::class, $contact);
+    }
+
+    /** @test */
+    public function contact_creation_has_event()
+    {
+        $dispatcher = app(EventDispatcher::class);
+
+        /*** assert ***/
+        $dispatcher->handle(ContactEvents::CREATED, function (ContactEvent $event) {
+            $this->assertTrue(true);
+        });
+
+        /*** act ***/
+        factory(Contact::class)->create();
     }
 }
