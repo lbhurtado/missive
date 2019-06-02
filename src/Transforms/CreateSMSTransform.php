266d69a5d433f452cc83444274333615e1e56205
Lester Hurtado <lester@hurtado.ph>
2019-06-02 20:58:25 +0800

    fix bug if relay is used in config

--- src/Transforms/CreateSMSTransform.php
+++ src/Transforms/CreateSMSTransform.php
@@ -3,6 +3,7 @@
 namespace LBHurtado\Missive\Transforms;
 
 use League\Tactician\Middleware;
+use LBHurtado\Missive\Facades\Missive;
 
 class CreateSMSTransform implements Middleware
 {
@@ -16,10 +17,17 @@ class CreateSMSTransform implements Middleware
 
     public function transform(array $attributes)
     {
-        return array_merge($attributes, [
+        $merged = array_merge($attributes, [
             'from' => phone($attributes['from'], 'PH')->formatE164(),
             'to' => phone($attributes['to'], 'PH')->formatE164(),
             'message' => trim($attributes['message']),
         ]);
+        $mapping = array_flip(Missive::getRelayProviderConfig());
+        $data = [];
+        foreach ($mapping as $key => $value) {
+        	$data[$key] = $merged[$value];
+        }
+
+        return $data;
     }
 }
